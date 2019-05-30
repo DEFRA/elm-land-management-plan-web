979ef2d1c818a43940d2946588a814d84a01f12c
David Bingham <david.bingham@defra.gov.uk>
2019-05-30 10:37:29 +0100

    Simplified api.get function.

--- server/plugins/api.js
+++ server/plugins/api.js
@@ -4,15 +4,13 @@ const config = require('../config')
 module.exports = {
   plugin: {
     name: 'api',
-    register: (server, options) => {
+    register: (server) => {
       server.method({
         name: 'api.get',
-        method: async (action) => {
-          return await request({
-            json: true,
-            uri: config.apibase + action
-          })
-        },
+        method: (action) => request({
+          json: true,
+          uri: config.apibase + action
+        }),
         options: {}
       })
     }
